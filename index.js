d7f3db14779d15a35f24efd46cb41d3ce36352d3
chrpac <chrpac@gmail.com>
2023-06-25 23:06:00 +0700

    function

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 var ffmpeg = require('fluent-ffmpeg');
 var config = require('./config')
 
-let encode = ffmpeg()
-    .input(config.url)
-    .inputOption([
-        "-vsync 0",
-        "-hwaccel cuvid",
-        "-hwaccel_device 0",
-        "-c:v h264_cuvid"
-    ])
-    .videoCodec("h264_nvenc")
-    .videoFilter("fps=5")
-    //.native()
-    //.audioCodec('aac')
-    //.audioBitrate(128)
-    //.audioChannels(2)
-    .videoBitrate(150)
-    .save("./output/out.mp4")
-    .on('progress', function (progress) {
-        console.log('Processing: ');
+function videoEncode(url) {
+    return new Promise((resolve, reject) => {
+        let encode = ffmpeg()
+            .input(url)
+            .inputOption([
+                "-vsync 0",
+                "-hwaccel cuvid",
+                "-hwaccel_device 0",
+                "-c:v h264_cuvid"
+            ])
+            .videoCodec("h264_nvenc")
+            .videoFilter("fps=5")
+            //.native()
+            //.audioCodec('aac')
+            //.audioBitrate(128)
+            //.audioChannels(2)
+            .videoBitrate(150)
+            .save("./output/out.mp4")
+            .on('progress', function (progress) {
+                console.log('Processing: ');
+            })
+            .on('error', (err) => {
+                //console.log(err)
+                reject(err)
+            })
+            .on('end', () => {
+                //console.log("done")
+                resolve('done');
+            });
+
+        setTimeout(function () {
+            encode.on('error', function () {
+                //console.log('Ffmpeg has been killed');
+                resolve('killed')
+            });
+
+            stop(encode);
+        }, 60000);
     })
-    .on('error', (err) => {
-        console.log(err)
-    })
-    .on('end', () => {
-        console.log("done")
-    });
+}
+
 
 const stop = (movie) => {
     return movie.ffmpegProc.stdin.write('q');
 }
 
-setTimeout(function () {
-    encode.on('error', function () {
-        console.log('Ffmpeg has been killed');
-    });
-
-    stop(encode);
-}, 60000);
+videoEncode(config.url)
+    .then(result => console.log(result))
+    .catch(error => console.log(error))
