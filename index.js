bbe7f2bbf371eba53fabd69405d635631d6ba03a
chrpac <chrpac@gmail.com>
2023-06-27 16:32:28 +0700

    adfa

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var ffmpeg = require('fluent-ffmpeg');
+var Ffmpeg = require('fluent-ffmpeg');
 var config = require('./config')
 
 function videoEncode(url, stream) {
     const link = url + stream + '.m3u8'
     return new Promise((resolve, reject) => {
-        let encode = ffmpeg()
+        let encode = new Ffmpeg()
             .input(link)
             .inputOption([
                 "-vsync 0",
@@ -22,6 +22,15 @@ function videoEncode(url, stream) {
             .save(`./output/${stream}.mp4`)
             .on('start', function () {
                 console.log(`Start Encoding: ${stream}`)
+                setTimeout(function () {
+                    encode.on('error', function () {
+                        //console.log('Ffmpeg has been killed');
+                        resolve('killed', stream)
+                    });
+
+                    //stop(encode);
+                    encode.ffmpegProc.stdin.write('q');
+                }, 30000);
             })
             .on('progress', function (progress) {
                 //console.log('Processing: ');
@@ -39,14 +48,7 @@ function videoEncode(url, stream) {
             return movie.ffmpegProc.stdin.write('q');
         }
 
-        setTimeout(function () {
-            encode.on('error', function () {
-                //console.log('Ffmpeg has been killed');
-                resolve('killed', stream)
-            });
 
-            stop(encode);
-        }, 30000);
     })
 }
 
@@ -57,7 +59,7 @@ function videoEncode(url, stream) {
 //     .then(result => console.log(result))
 //     .catch(error => console.log(error))
 
-for (let i = 1; i < 20; i++) {
+for (let i = 1; i < 5; i++) {
     const num = '000' + i;
     const sub = num.length <= 4 ? num : num.substring(num.length - 4)
     videoEncode(config.url, sub)
