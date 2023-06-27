84e7fafc80ecee5a953822514ccb5ffdb1677f2a
chrpac <chrpac@gmail.com>
2023-06-27 16:34:29 +0700

    adfad

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function videoEncode(url, stream) {
 //     .then(result => console.log(result))
 //     .catch(error => console.log(error))
 
-for (let i = 1; i < 5; i++) {
+for (let i = 1; i < 20; i++) {
     const num = '000' + i;
     const sub = num.length <= 4 ? num : num.substring(num.length - 4)
     videoEncode(config.url, sub)
